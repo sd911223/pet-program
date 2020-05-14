45ee808747362c9e5f1494248b748b2c9b6d0dcd
shidungit2019 <460475302@qq.com>
2020-05-14 23:46:07 +0800

    提交框架相关

---
+++ src/main/java/com/live/PlatformRenewApplication.java
@@ -0,0 +1,13 @@
+package com.live;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlatformRenewApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlatformRenewApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/live/config/SwaggerConfig.java
@@ -0,0 +1,43 @@
+package com.live.config;
+
+import com.github.xiaoymin.knife4j.spring.annotations.EnableKnife4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ * @author shidun
+ */
+@Configuration
+@EnableSwagger2
+@EnableKnife4j
+@Profile("dev")
+public class SwaggerConfig {
+    @Bean
+    public Docket createRestApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.live.contorller"))
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("Spring Boot中使用Swagger2构建RESTful APIs")
+                .version("1.0")
+                .description("后台接口")
+                .termsOfServiceUrl("127.0.0.1:8081/doc.html")
+                .contact(new Contact("SB", "www.google.com", "buxiangshuo@qq.com"))
+                .build();
+    }
+}
