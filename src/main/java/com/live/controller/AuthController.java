ef79f3db10d6091d28459bc806be57883cb21ea6
dun.shi <sd123456>
2020-05-15 14:06:24 +0800

    增加redis工具类,(删除yml注释,启动会报错,字符格式问题)

--- src/main/java/com/live/controller/AuthController.java
+++ src/main/java/com/live/controller/AuthController.java
@@ -3,6 +3,8 @@ package com.live.controller;
 import com.live.common.RestResponse;
 import com.live.common.ResultUtil;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,9 +17,19 @@ import org.springframework.web.bind.annotation.RestController;
 @Api(tags = "小程序授权登录")
 @RequestMapping("/api/wechat")
 public class AuthController {
+
     @ApiOperation("授权")
     @PostMapping("/mp_auth")
-    public RestResponse mp_auth() {
+    @ApiImplicitParams({@ApiImplicitParam(name = "spread_spid", example = "0"),
+            @ApiImplicitParam(value = "spread_code", required = true),
+            @ApiImplicitParam(name = "iv", required = true),
+            @ApiImplicitParam(name = "encryptedData", required = true),
+            @ApiImplicitParam(name = "code", required = true),
+            @ApiImplicitParam(name = "cache_key", required = true),
+            @ApiImplicitParam(name = "login_type", example = "routine")
+    })
+    public RestResponse mp_auth(String spread_spid,String spread_code,String iv,String encryptedData,String code,String cache_key,String login_type) {
+
         return ResultUtil.success();
     }
 }
