aa65a264c038b3f82a0ec32473553e0b6f412d05
shidungit2019 <460475302@qq.com>
2020-05-16 19:09:34 +0800

    直播列表

--- src/main/java/com/live/service/impl/LiveServiceImpl.java
+++ src/main/java/com/live/service/impl/LiveServiceImpl.java
@@ -2,6 +2,12 @@ package com.live.service.impl;
 
 import com.alibaba.fastjson.JSONObject;
 import com.live.common.RestResponse;
+import com.live.dao.LiveStoreClerkMapper;
+import com.live.dao.LiveStoreMapper;
+import com.live.dao.LiveUserMapper;
+import com.live.model.LiveStore;
+import com.live.model.LiveStoreClerk;
+import com.live.model.LiveStoreKey;
 import com.live.model.LiveUser;
 import com.live.service.IRedisService;
 import com.live.service.LiveService;
@@ -17,13 +23,36 @@ import org.springframework.stereotype.Service;
 public class LiveServiceImpl implements LiveService {
     @Autowired
     private IRedisService redisService;
+    @Autowired
+    LiveUserMapper liveUserMapper;
+    @Autowired
+    LiveStoreClerkMapper liveStoreClerkMapper;
+    @Autowired
+    LiveStoreMapper liveStoreMapper;
 
     @Override
     public RestResponse getLiveList(Integer page, Integer store_id, String token) {
         //根据token获取uid
         Object value = redisService.getToken(token);
         LiveUser user = JSONObject.parseObject(value.toString(), LiveUser.class);
+        LiveUser liveUser = liveUserMapper.selectByPrimaryKey(user.getUid());
+        if (liveUser.getClerk_id() != null) {
+            getLiveStore(liveUser.getClerk_id());
+        }
         return null;
     }
 
+    /**
+     * 获取绑定店铺
+     *
+     * @param clerkId
+     * @return
+     */
+    public LiveStore getLiveStore(Integer clerkId) {
+        LiveStoreClerk clerk = liveStoreClerkMapper.selectByPrimaryKey(clerkId);
+        LiveStoreKey liveStoreKey = new LiveStoreKey();
+        liveStoreKey.setStore_no(clerk.getStore_id().toString());
+        LiveStore liveStore = liveStoreMapper.selectByPrimaryKey(liveStoreKey);
+        return liveStore;
+    }
 }
